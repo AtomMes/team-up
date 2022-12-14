1b06dd5d9bf91f08f7e5e185cab5433903c0c3af
AtomMes <atomik.2005@mail.ru>
2022-12-14 22:19:39 +0400

    button changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,104 +1,112 @@
-import { Box, Button } from '@mui/material';
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.scss';
+import { Box, Button } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.scss";
 
 const Navbar = () => {
   const navigationButtons = [
     {
-      text: 'Home',
-      to: '/',
+      text: "Home",
+      to: "/",
     },
     {
-      text: 'Community',
-      to: '/',
+      text: "Community",
+      to: "/",
     },
     {
-      text: 'Games',
-      to: '/',
+      text: "Games",
+      to: "/",
     },
     {
-      text: 'Quizz',
-      to: '/',
+      text: "Quizz",
+      to: "/",
     },
   ];
 
   return (
-    <Box maxWidth="1130px" width="100%" margin="20px auto" bgcolor="transparent">
-      <Box display="flex" justifyContent="space-between">
+    <Box bgcolor="#e8e8e8" padding="20px 0">
+      <Box
+        maxWidth="1130px"
+        width="100%"
+        margin="0 auto"
+        bgcolor="transparent"
+        display="flex"
+        justifyContent="space-between"
+      >
         <Box display="flex">
           <Button
             variant="outlined"
             sx={{
-              fontWeight: '600',
-              fontSize: '20px',
-              color: 'white',
-              backgroundColor: '#ea552b',
-              width: '150px',
-              minHeight: '60px',
-              whiteSpace: 'nowrap',
-              marginRight: '20px',
-              border: 'transparent',
-              '&:hover': {
-                border: 'transparent',
-                backgroundColor: '#e34114',
+              fontWeight: "600",
+              fontSize: "20px",
+              color: "white",
+              backgroundColor: "#ea552b",
+              width: "150px",
+              minHeight: "60px",
+              whiteSpace: "nowrap",
+              marginRight: "20px",
+              border: "transparent",
+              "&:hover": {
+                border: "transparent",
+                backgroundColor: "#e34114",
               },
-            }}>
+            }}
+          >
             Logo
           </Button>
           <Box display="flex" gap="30px">
             {navigationButtons.map((nav, i) => (
               <Button
                 key={i}
-                className="navigationButton"
+                className="slide"
                 sx={{
-                  color: '#181818',
-                  textTransform: 'capitalize',
-                  lineHeight: '40px',
-                  padding: '0 15px',
-                  fontSize: '20px',
-                  '&:hover': {
-                    color: 'white',
-                  },
-                }}>
+                  color: "#181818",
+                  textTransform: "capitalize",
+                  lineHeight: "40px",
+                  padding: "0 15px",
+                  fontSize: "20px",
+                }}
+              >
                 {nav.text}
               </Button>
             ))}
           </Box>
         </Box>
-        <Box display="flex" sx={{ minHeight: '60px' }} alignItems="center">
+        <Box display="flex" sx={{ minHeight: "60px" }} alignItems="center">
           <Link to="/login">
             <Button
               sx={{
-                color: '#181818',
-                textTransform: 'capitalize',
-                fontSize: '16px',
-                marginRight: '30px',
-                fontWeight: '500',
-              }}>
+                color: "#181818",
+                textTransform: "capitalize",
+                fontSize: "16px",
+                marginRight: "30px",
+                fontWeight: "500",
+              }}
+            >
               Login
             </Button>
           </Link>
           <Button
             variant="outlined"
             sx={{
-              fontSize: '16px',
-              textTransform: 'capitalize',
-              color: 'white',
-              backgroundColor: '#181818',
-              width: '150px',
-              minHeight: '60px',
-              '&:hover': {
-                backgroundColor: 'primary.dark',
+              fontSize: "16px",
+              textTransform: "capitalize",
+              color: "white",
+              backgroundColor: "#181818",
+              width: "150px",
+              minHeight: "60px",
+              "&:hover": {
+                backgroundColor: "primary.dark",
               },
-              whiteSpace: 'nowrap',
-              marginRight: '20px',
-              border: 'transparent',
-              '&:hover': {
-                border: 'transparent',
-                backgroundColor: '#e34114',
+              whiteSpace: "nowrap",
+              marginRight: "20px",
+              border: "transparent",
+              "&:hover": {
+                border: "transparent",
+                backgroundColor: "#e34114",
               },
-            }}>
+            }}
+          >
             Try for free
           </Button>
         </Box>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.scss';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.scss";
+import { BrowserRouter } from "react-router-dom";
+import Countdown from "react-countdown";
+import { Box } from "@mui/material";
+import countdownImage from "./img/countdown.jpg";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  // <BrowserRouter>
+  //   <App />
+  // </BrowserRouter>
+  <div
+  className="container"
+    
+  >
+    <Countdown className="countdown" date={Date.now() + 280000000} />
+    <p className="comingSoon" >Coming Soon</p>
+  </div>
 );
