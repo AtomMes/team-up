aebb6b8259d33a17c7e641631e1fffa456005f2c
tyommmm <artyomhovsepyan135@gmail.com>
2022-12-15 01:28:34 +0400

    countdown added

---
+++ src/components/CountDown.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const CountDown = () => {
+  const [width, setWidth] = React.useState(400);
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setWidth(width + 1);
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, [width]);
+  console.log('render');
+  return (
+    <div className="container">
+      <p className="daysLeft">5 Days left...</p>
+
+      <div className="countdown">
+        <div className="loading" style={{ width: `${String(width + 'px')}` }}></div>
+      </div>
+      <p className="comingSoon">Coming Soon</p>
+    </div>
+  );
+};
+
+export default CountDown;
